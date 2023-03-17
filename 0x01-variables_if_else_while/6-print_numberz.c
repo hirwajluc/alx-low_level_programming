65384aae00b007f92ace46f34df021b29dc9e90a
Hirwa Jean Luc <hirwajluc@gmail.com>
2023-03-17 13:06:19 -0700

    Print Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: HIRWA Jean Luc
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48 ; a < 58 ; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
