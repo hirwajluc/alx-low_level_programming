06969f36fb0492ab43519d4941e01cec07dbdbdf
Hirwa Jean Luc <hirwajluc@gmail.com>
2023-03-17 13:04:25 -0700

    Print Number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: HIRWA Jean Luc
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
