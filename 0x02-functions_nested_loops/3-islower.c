b733af94531a3815c5a2b9e47c1cf24144830242
Hirwa Jean Luc <hirwajluc@gmail.com>
2023-03-21 12:08:42 -0700

    isLower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
  * _islower - checks if a character is lower case if YES it return 1
  * otherwise returns 0 just like islower() function is ctype.h
  * @c: Is the int value to be compared with the ASCII value
+ * author: Hirwa
  * Return: 0 if successful.
  */
 int _islower(int c)
