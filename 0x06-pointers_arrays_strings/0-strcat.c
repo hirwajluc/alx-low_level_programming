6bf0c4206267c7291b564bc9055ee71544d9ddcd
Hirwa Jean Luc <hirwajluc@gmail.com>
2023-03-30 13:46:08 -0700

    MorePointersArraysStrings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,7 @@
  * _strcat - concatenates two strings
  * @dest: string to append to
  * @src: string to add
- *
+ * author: Hirwa
  * Return: a pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,7 +5,7 @@
  * @dest: destination string
  * @src: source string
  * @n: number of bytes of str to concatenate
- *
+ * author: Hirwa
  * Return: a pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
